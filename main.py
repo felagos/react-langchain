f42a41370c69dda6b40b909575e5002528f71a19
Felipe Andres Lagos Sanchez <flagos@internacional.cl>
2024-04-01 14:49:47 -0300

    Add tool and prompt template for text length calculation

--- main.py
+++ main.py
@@ -1,10 +1,45 @@
 from dotenv import load_dotenv
+from langchain.agents import tool
+from langchain.prompts import PromTemplate
+from langchain.tools.render import render_text_description
 
 load_dotenv()
 
+
+@tool
 def get_text_length(text: str) -> int:
     """Return the length of the input text by characters."""
     return len(text)
 
+
 if __name__ == "__main__":
-    pass
+    tools = [get_text_length]
+
+    template = """
+    Answer the following questions as best you can. You have access to the following tools:
+
+    {tools}
+
+    Use the following format:
+
+    Question: the input question you must answer
+    Thought: you should always think about what to do
+    Action: the action to take, should be one of [{tool_names}]
+    Action Input: the input to the action
+    Observation: the result of the action
+    ... (this Thought/Action/Action Input/Observation can repeat N times)
+    Thought: I now know the final answer
+    Final Answer: the final answer to the original input question
+
+    Begin!
+
+    Question: {input}
+    Thought
+    """
+
+    prompt = PromTemplate.from_template(
+        template=template
+    ).partial(
+        tools=render_text_description(tools), 
+        tool_names=", ".join([t.name for t in tools])
+    )
